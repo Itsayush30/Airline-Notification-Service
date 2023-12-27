f929b83981849c18f198b5219ff156721ed21308
itsayush30 <itsayush30@gmail.com>
2023-12-27 18:24:43 +0530

    Created nodemailer transport object and tried

--- src/config/server-config.js
+++ src/config/server-config.js
@@ -3,5 +3,8 @@ const dotenv = require('dotenv'); //it will provide dotenv module object
 dotenv.config(); // calling config function from object
 
 module.exports = {
-    PORT: process.env.PORT
+    PORT: process.env.PORT,
+    GMAIL_PASS: process.env.GMAIL_PASS,
+    GMAIL_EMAIL: process.env.GMAIL_EMAIL,
+
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
-const express = require('express');
+const express = require("express");
 
-const { ServerConfig } = require('./config');
-const apiRoutes = require('./routes');
+const { ServerConfig } = require("./config");
+const apiRoutes = require("./routes");
+
+const mailSender = require("./config/email-config");
 
 const app = express();
 
-app.use('/api', apiRoutes);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.listen(ServerConfig.PORT, () => {
-    console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
+app.use("/api", apiRoutes);
+
+app.listen(ServerConfig.PORT, async () => {
+  console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
+  try {
+    const response = await mailSender.sendMail({
+      from: ServerConfig.GMAIL_EMAIL,
+      to: "itsayush30@gmail.com",
+      subject: "Is this working ?",
+      text: "Yes it is working",
+    });
+    console.log(response);
+  } catch (error) {
+    console.log(error);
+  }
 });
